cc6a3530e320e481fd9b76d68d47978df366f2bd
Natalie Dixon <dixonnataliej@gmail.com>
2025-07-24 16:20:59 -0600

    Seed database with default sounds

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+default_sounds = [
+  { name: 'Airhorn', file_path: 'airhorn.mp3' },
+  { name: 'Anime Wow', file_path: 'anime-wow.mp3' },
+  { name: 'Applause', file_path: 'applause.mp3' },
+  { name: 'Background Music', file_path: 'bg-music.mp3' },
+  { name: 'Crickets', file_path: 'crickets.mp3' },
+  { name: 'Drumroll', file_path: 'drumroll.mp3' },
+  { name: 'Explosion', file_path: 'explosion.mp3' },
+  { name: 'Quack', file_path: 'quack.mp3' },
+  { name: 'Splat', file_path: 'splat.mp3' },
+  { name: 'Yippee', file_path: 'yippee.mp3' }
+]
+
+default_sounds.each do |sound|
+  Sound.find_or_create_by!(name: sound[:name]) do |s|
+    s.audio_file.attach(io: File.open(Rails.root.join("db", "seeds", "audio", sound[:file_path])), filename: sound[:file_path])
+  end
+end
