9f42c2a49f84029d479c79922d32cd37f713a8c1
Natalie Dixon <dixonnataliej@gmail.com>
2025-07-25 17:57:07 -0600

    [#42] Add color and emoji to sounds

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_23_151941) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_25_220723) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -47,6 +47,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_23_151941) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "color"
+    t.string "emoji"
     t.index ["user_id"], name: "index_sounds_on_user_id"
   end
 
