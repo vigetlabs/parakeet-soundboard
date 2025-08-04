2e230d062d0aa8ebf580a2b207ba38c8722843b5
Natalie Dixon <dixonnataliej@gmail.com>
2025-08-04 13:33:56 -0600

    Style fix

--- api/db/migrate/20250804180359_scope_folder_slug_to_user.rb
+++ api/db/migrate/20250804180359_scope_folder_slug_to_user.rb
@@ -1,6 +1,6 @@
 class ScopeFolderSlugToUser < ActiveRecord::Migration[8.0]
   def change
     remove_index :folders, :slug
-    add_index :folders, [:user_id, :slug], unique: true
+    add_index :folders, [ :user_id, :slug ], unique: true
   end
 end
