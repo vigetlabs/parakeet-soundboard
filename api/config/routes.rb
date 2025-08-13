e6d472d83f5e7128e0266017db2db00c479a7f83
Natalie Dixon <dixonnataliej@gmail.com>
2025-08-13 10:53:30 -0600

    Show user action to check if token is valid

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
     sessions: "users/sessions",
     registrations: "users/registrations"
   }
+  devise_scope :user do
+    get "users/show", to: "users/sessions#show"
+  end
   resources :sounds do
     member do
       get "in_folders"
