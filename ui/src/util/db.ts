4255c004425a1391cd3a232a8ef957d2b19794a6
Nevin Morgan <nevin@longtowel.com>
2025-08-14 12:37:08 -0400

    fix: update api url to exclude port if empty

--- ui/src/util/db.ts
+++ ui/src/util/db.ts
@@ -1,7 +1,7 @@
-import { QueryClient } from "@tanstack/react-query";
+import { QueryClient } from '@tanstack/react-query';
 
 export const queryClient = new QueryClient({});
 
-export const API_URL = `${import.meta.env.VITE_API_HOST}:${
-  import.meta.env.VITE_API_PORT
+export const API_URL = `${import.meta.env.VITE_API_HOST}${
+  import.meta.env.VITE_API_PORT ? `:${import.meta.env.VITE_API_PORT}` : ''
 }`;
