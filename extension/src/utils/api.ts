86e154fffe9cebf8ec82ef3b6c77618b6d358dd6
Rembi <rembitime@gmail.com>
2025-08-15 11:11:23 -0600

    Fixed extension API path

--- extension/src/utils/api.ts
+++ extension/src/utils/api.ts
@@ -1,5 +1,5 @@
-export const API_URL = `${import.meta.env.VITE_API_HOST}:${
-  import.meta.env.VITE_API_PORT
+export const API_URL = `${import.meta.env.VITE_API_HOST}${
+  import.meta.env.VITE_API_PORT ? `:${import.meta.env.VITE_API_PORT}` : ""
 }`;
 
 async function fetchWithAuth(
